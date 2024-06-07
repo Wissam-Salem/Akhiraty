573e89a2394c27c2775e3034ee69691b89929827
Kitler1 <104741326+Kitler1@users.noreply.github.com>
2024-06-07 04:18:19 +0300

    committed

--- src/components/Ayah/Ayah.jsx
+++ src/components/Ayah/Ayah.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Ayah({ ayah, ayah_id }) {
+  const ayahAudio = useRef(null);
+  const { id } = useParams();
+  const handleAyahPlay = () => {
+    if (ayahAudio.current.paused === true) {
+      ayahAudio.current.play();
+    }
+  };
+
   return (
-    <div className="ayah gap-2 my-5 max-sm:my-3 text-3xl max-sm:text-2xl" dir="rtl">
+    <a
+      onClick={() => {
+        handleAyahPlay();
+      }}
+      className="ayah gap-2 my-5 max-sm:my-3 text-3xl max-sm:text-2xl"
+      dir="rtl"
+    >
       <span className="w-fit gap-2 after:content-['\06DD']">{ayah} </span>
       {ayah_id}
-    </div>
+      <audio
+        preload="auto"
+        ref={ayahAudio}
+        src={`https://quranaudio.pages.dev/2/${id}_${ayah_id}.mp3`}
+      ></audio>
+    </a>
   );
 }
